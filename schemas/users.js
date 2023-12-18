53646c67039c2e8b0926b59998ffeaada731f27b
pnyekrasov <pnekrasov1910@gmail.com>
2023-12-18 02:36:12 +0200

    h 2

--- schemas/users.js
+++ schemas/users.js
@@ -6,7 +6,7 @@ const validKefs = [1.2, 1.375, 1.55, 1.725, 1.9];
 
 const validGoals = ["lose fat", "maintain", "gain muscle"];
 
-const registerSchema = Joi.object({
+const registerAndLoginSchema = Joi.object({
   name: Joi.string()
     .required("Set name for user")
     .pattern(
@@ -53,7 +53,7 @@ const updateUserSchema = Joi.object({
 });
 
 module.exports = {
-  registerAndLoginSchema: registerSchema,
+  registerAndLoginSchema,
   loginSchema,
   emailSchema,
   updateUserSchema,
