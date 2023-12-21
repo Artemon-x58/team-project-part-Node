a88711ab36de9efd6a3c0c40939bd19e7e6f4b45
Artemon-x58 <artem1145819@gmail.com>
2023-12-21 16:07:10 +0100

    update addDiary

--- schemas/addDiary.js
+++ schemas/addDiary.js
@@ -2,17 +2,19 @@ const Joi = require("joi");
 
 const validMeals = ["breakfast", "dinner", "lunch", "snack"];
 
-const DairySchema = Joi.array().items(
-  Joi.object({
-    meals: Joi.string()
-      .valid(...validMeals)
-      .required(),
-    title: Joi.string().required(),
-    calories: Joi.number().min(1).max(10000).required(),
-    carbohydrates: Joi.number().min(1).max(6000).required(),
-    protein: Joi.number().min(1).max(2000).required(),
-    fat: Joi.number().min(1).max(2000).required(),
-  })
-);
+const DairySchema = Joi.object({
+  meals: Joi.string()
+    .valid(...validMeals)
+    .required(),
+  entries: Joi.array().items(
+    Joi.object({
+      title: Joi.string().required(),
+      calories: Joi.number().min(1).max(10000).required(),
+      carbohydrates: Joi.number().min(1).max(6000).required(),
+      protein: Joi.number().min(1).max(2000).required(),
+      fat: Joi.number().min(1).max(2000).required(),
+    })
+  ),
+});
 
 module.exports = DairySchema;
