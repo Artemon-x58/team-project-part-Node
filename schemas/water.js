60fc0426e8c450402b41cf12b6dda4ade013b044
Artemon-x58 <artem1145819@gmail.com>
2023-12-22 18:23:41 +0100

    water integer

--- schemas/water.js
+++ schemas/water.js
@@ -1,7 +1,7 @@
 const Joi = require("joi");
 
 const waterSchema = Joi.object({
-  water: Joi.number().min(1).max(3000).required(),
+  water: Joi.number().integer().min(1).max(3000).required(),
 });
 
 module.exports = waterSchema;
