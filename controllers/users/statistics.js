7899c42a7760aa8580b9b2e7cbf076888562c256
Artemon-x58 <artem1145819@gmail.com>
2023-12-23 13:38:45 +0100

    test params

--- controllers/users/statistics.js
+++ controllers/users/statistics.js
@@ -7,7 +7,7 @@ const {
 const { Calories, Weight, Water } = require("../../models");
 
 const statistics = async (req, res) => {
-  const { month } = req.body;
+  const { month } = req.params;
   const { id: owner } = req.user;
 
   const monthNumber = getMonthNumber(month);
